c3a6d2681fe903f762608b4ce53bae9108bb2d0a
Christoph Döpmann <christoph.doepmann@tu-berlin.de>
2022-03-30 09:39:23 +0200

    First doc parsing approach.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod parser;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Input consensus to sample from.
+    #[clap(long)]
+    consensus: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let mut consensus_raw = String::new();
+    let mut file = File::open(cli.consensus).unwrap();
+    file.read_to_string(&mut consensus_raw).unwrap();
+
+    // run our parser tests
+    println!("{:?}", parser::parse_consensus(&consensus_raw));
 }
